5760e5eace29213281a1e6d129d4a7b42febb41e
annadowling <anna.dowling@errigal.com>
2020-03-09 13:00:17 +0000

    Added more GCs

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
-JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseG1GC -Djava.library.path=/usr/local/lib"
-#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseG1GC -Djava.library.path=/usr/local/lib"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+USeParNewGC -Djava.library.path=/usr/local/lib"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseSerialGC -Djava.library.path=/usr/local/lib"
+#JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UseParallelGC -Djava.library.path=/usr/local/lib"
+JAVA_OPTS="$JAVA_OPTS -Xms1G -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC"
 export JAVA_OPTS
 
 java $JAVA_OPTS -jar jzmq-consumer.jar &
