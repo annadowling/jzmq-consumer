c0bafa9fcc77053dd4feed517efea2b840b86367
annadowling <anna.dowling@errigal.com>
2020-03-13 13:38:50 +0000

    added multik threaded support

--- src/main/java/com/msc/spring/consumer/jzmq/JZMQSubscriber.java
+++ src/main/java/com/msc/spring/consumer/jzmq/JZMQSubscriber.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.zeromq.ZMQ;
 
@@ -21,6 +22,9 @@ public class JZMQSubscriber {
 
     @Value("${jzmq.enabled}")
     private boolean jzmqEnabled;
+
+    @Value("${multi.thread.enabled}")
+    private boolean multiThreaded;
 
     final String errorMessage = "Exception encountered = ";
 
@@ -41,16 +45,25 @@ public class JZMQSubscriber {
                 System.out.println("Starting Subscriber..");
                 int i = 0;
                 while (true) {
-                    String message = subscriber.recvStr();
-                    byte[] messageBody = subscriber.recv();
-
-                    messageUtils.saveMessage(messageBody);
-                    System.out.println(" [x] Received Message: '" + message + "'");
+                    if(multiThreaded){
+                        receiveMessageMultiThread(subscriber);
+                    }else{
+                        byte[] messageBody = subscriber.recv();
+                        messageUtils.saveMessage(messageBody);
+                        System.out.println(" [x] Received Message: '" + messageBody + "'");
+                    }
                     i++;
                 }
             } catch (Exception e) {
                 System.out.println(errorMessage + e.getLocalizedMessage());
             }
         }
+    }
+
+    @Async
+    void receiveMessageMultiThread(ZMQ.Socket subscriber){
+        byte[] messageBody = subscriber.recv();
+        messageUtils.saveMessage(messageBody);
+        System.out.println(" [x] Received Message: '" + messageBody + "'");
     }
 }
